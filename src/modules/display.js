c93e7c8fb5c72579a0b661b3a591e8f70e8178cd
Your Name <legend.it.bk.ru>
2022-02-03 00:57:06 +0500

    Fix linter issues ESLint

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,4 +1,5 @@
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Vgufs5n18vzBAjzxI4Io/scores';
+const url =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Vgufs5n18vzBAjzxI4Io/scores';
 
 const scoreDisp = () => document.querySelector('#list-scores');
 
@@ -21,7 +22,6 @@ const scoresDisplay = (scores) => {
 const getScoreObjects = async () => {
   const response = await fetch(url);
   const scores = (await response.json()).result;
-  // console.log(scores);
 
   scoresDisplay(scores);
 };
@@ -46,14 +46,12 @@ window.addEventListener('load', () => {
 addRefresh();
 
 const postScore = (scoreObject) => {
-// console.log('start post score fun');
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-  body: JSON.stringify(scoreObject),
-
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(scoreObject),
   });
 };
 
@@ -68,12 +66,10 @@ const submitAddFunc = () => {
       user: userInput.value,
       score: formData.get('score'),
     };
-    // console.log('before post score fun');
+
     postScore(object);
-    // console.log('after post scrore fun');
     form.reset();
   });
 };
 
 submitAddFunc();
-// console.log('after submitAddFunc function');
